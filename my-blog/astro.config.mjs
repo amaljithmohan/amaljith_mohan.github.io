bc5c113608f2fdb0ee63fb8f37a6d242df1e6b49
AJM <40321303+amaljithmohan@users.noreply.github.com>
2026-03-25 12:17:15 +0000

    Remove base path after repo rename

--- my-blog/astro.config.mjs
+++ my-blog/astro.config.mjs
@@ -3,9 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://amaljithmohan.github.io',
-	base: '/amaljith_mohan.github.io',
 	integrations: [mdx(), sitemap()],
 });
